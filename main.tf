44422f5453601dfafcd75dad7587e1f7129ac667
laslopaul <96680549+laslopaul@users.noreply.github.com>
2023-04-01 17:01:13 +0100

    Add module reference

--- main.tf
+++ main.tf
@@ -6,3 +6,15 @@ terraform {
     }
   }
 }
+
+module "aws-simple-network" {
+  source      = "./modules/aws-simple-network"
+  vpc_cidr    = "10.0.0.0/16"
+  region      = "eu-west-2"
+  environment = "staging"
+}
+
+output "vpc_id" {
+  description = "VPC identifier"
+  value       = module.aws-simple-network.vpc_id
+}
