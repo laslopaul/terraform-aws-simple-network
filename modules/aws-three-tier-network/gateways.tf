4223b488b2540fdbff238013bf79cedc56138fed
laslopaul <96680549+laslopaul@users.noreply.github.com>
2023-04-18 14:36:22 +0100

    Change NAT connectivity to public

--- modules/aws-three-tier-network/gateways.tf
+++ modules/aws-three-tier-network/gateways.tf
@@ -2,10 +2,22 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.default.id
 }
 
+resource "aws_eip" "nat" {
+  count = local.subnets_per_tier
+  vpc   = true
+  tags = {
+    type = "nat"
+  }
+}
+
 resource "aws_nat_gateway" "nat" {
   count             = local.subnets_per_tier
-  connectivity_type = "private"
+  connectivity_type = "public"
+  allocation_id     = aws_eip.nat[count.index].id
   subnet_id         = aws_subnet.public[count.index].id
+
+  depends_on = [aws_internet_gateway.main]
+
   tags = {
     az = aws_subnet.public[count.index].tags_all["az"]
   }
