d9c92bddde033d7100f1fd47e6910f79c4fd408a
laslopaul <96680549+laslopaul@users.noreply.github.com>
2023-04-18 16:49:43 +0100

    Add security groups

--- main.tf
+++ main.tf
@@ -8,8 +8,9 @@ terraform {
 }
 
 module "aws-three-tier-network" {
-  source      = "./modules/aws-three-tier-network"
-  vpc_cidr    = "10.0.0.0/16"
-  region      = "eu-west-2"
-  environment = "staging"
+  source              = "./modules/aws-three-tier-network"
+  vpc_cidr            = "10.0.0.0/16"
+  region              = "eu-west-2"
+  bastion_access_cidr = "0.0.0.0/0"
+  environment         = "staging"
 }
