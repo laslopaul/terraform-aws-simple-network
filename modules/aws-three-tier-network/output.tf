93660a698d7cedc7217860a65610e07f66a2f51c
laslopaul <96680549+laslopaul@users.noreply.github.com>
2023-04-18 17:04:30 +0100

    Change module outputs

--- modules/aws-three-tier-network/output.tf
+++ modules/aws-three-tier-network/output.tf
@@ -4,27 +4,52 @@ output "vpc_id" {
 }
 
 output "subnets_web_tier" {
-  description = "Map of web-tier subnet CIDRs and their availability zones"
-  value = {
-    for s in aws_subnet.public :
-    s.availability_zone => s.cidr_block
-  }
+  description = "List of web-tier subnet ids"
+  value       = [for s in aws_subnet.public : s.id]
 }
 
 output "subnets_app_tier" {
-  description = "Map of app-tier subnet CIDRs and their availability zones"
-  value = {
-    for s in aws_subnet.private :
-    s.availability_zone => s.cidr_block
+  description = "List of app-tier subnet ids"
+  value = [
+    for s in aws_subnet.private : s.id
     if s.tags["tier"] == "app"
-  }
+  ]
 }
 
 output "subnets_db_tier" {
-  description = "Map of database-tier subnet CIDRs and their availability zones"
-  value = {
-    for s in aws_subnet.private :
-    s.availability_zone => s.cidr_block
+  description = "List of database-tier subnet ids"
+  value = [
+    for s in aws_subnet.private : s.id
     if s.tags["tier"] == "db"
-  }
+  ]
+}
+
+output "sg_bastion" {
+  description = "Id of Bastion security group"
+  value       = aws_security_group.sg_bastion.id
+}
+
+output "sg_external_lb" {
+  description = "Id of internet-facing load balancer security group"
+  value       = aws_security_group.sg_external_lb.id
+}
+
+output "sg_internal_lb" {
+  description = "Id of internal load balancer security group"
+  value       = aws_security_group.sg_internal_lb.id
+}
+
+output "sg_web_tier" {
+  description = "Id of web-tier security group"
+  value       = aws_security_group.sg_web_tier.id
+}
+
+output "sg_app_tier" {
+  description = "Id of app-tier security group"
+  value       = aws_security_group.sg_app_tier.id
+}
+
+output "sg_db_tier" {
+  description = "Id of database-tier security group"
+  value       = aws_security_group.sg_db_tier.id
 }
