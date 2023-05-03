beedb99313dfcf737b4361807566ebfb2be466e6
laslopaul <96680549+laslopaul@users.noreply.github.com>
2023-05-03 13:14:21 +0100

    Edit variables description

--- modules/aws-three-tier-db/variables.tf
+++ modules/aws-three-tier-db/variables.tf
@@ -4,22 +4,22 @@ variable "db_name" {
 }
 
 variable "db_instance_class" {
-  description = "Instance class of the database"
+  description = "The instance type of the RDS instance"
   type        = string
 }
 
 variable "db_storage_size" {
-  description = "Size of database storage in gibibytes"
+  description = "The allocated RDS instance storage in gibibytes"
   type        = number
 }
 
 variable "db_subnets" {
-  description = "List of VPC subnet ids for the database"
+  description = "List of VPC subnets to associate with the RDS instance"
   type        = list(string)
 }
 
 variable "db_security_group" {
-  description = "Id of the database security group"
+  description = "VPC security group to associate with the RDS instance"
   type        = string
 }
 
@@ -30,12 +30,17 @@ variable "db_username" {
 }
 
 variable "db_password" {
-  description = "RDS instance root username"
+  description = "RDS instance root password"
   type        = string
   sensitive   = true
 }
 
 variable "backup_retention_period" {
-  description = " The days to retain database backups for"
+  description = "The number of days to retain database backups for"
   type        = number
+}
+
+variable "azs" {
+  description = "List of availability zones (primary for the main DB and secondary for the replica)"
+  type        = list(string)
 }
