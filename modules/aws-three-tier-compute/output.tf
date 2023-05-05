b18bbd7fab3ef5554af54a0cb767f6994c97619c
laslopaul <96680549+laslopaul@users.noreply.github.com>
2023-05-06 00:48:50 +0100

    Change outputs of the compute module

--- modules/aws-three-tier-compute/output.tf
+++ modules/aws-three-tier-compute/output.tf
@@ -4,11 +4,11 @@ output "bastion_public_ip" {
 }
 
 output "web_nodes_asg" {
-  description = "Web-tier autoscaling group"
-  value       = aws_autoscaling_group.web_nodes
+  description = "Identifier of web-tier autoscaling group"
+  value       = aws_autoscaling_group.web_nodes.id
 }
 
 output "app_nodes_asg" {
-  description = "App-tier autoscaling group"
-  value       = aws_autoscaling_group.app_nodes
+  description = "Identifier of app-tier autoscaling group"
+  value       = aws_autoscaling_group.app_nodes.id
 }
